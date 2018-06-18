5bc420a81eebbd96af0b958344a8d0c7538f0ba0
Andrew Delapaz <ardelapaz@yahoo.com>
2018-06-18 19:47:06 -0400

    Stripe with private wiki integration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
     def demote
         current_user.member!
         current_user.paid = false
+        current_user.wikis.each do |wiki|
+            wiki.is_private = false
+            wiki.save!
+        end
         flash[:notice] = "Account downgraded successfully. We hope to see you back again soon!"
         redirect_to wikis_url  
       end
